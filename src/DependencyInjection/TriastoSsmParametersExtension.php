fcb0f7807fe0cb82085eb6240f75193480f5299e
Lucas <lucas.pekel@gmail.com>
2021-04-13 11:05:02 +0200

    Adjusted dependency injection to meet the correct classes and services.yaml location

--- src/DependencyInjection/TriastoSsmParametersExtension.php
+++ src/DependencyInjection/TriastoSsmParametersExtension.php
@@ -4,17 +4,19 @@ declare(strict_types=1);
 
 namespace Triasto\SsmParametersBundle\DependencyInjection;
 
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 class TriastoSsmParametersExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
-        $loader = new XmlFileLoader(
+        $loader = new YamlFileLoader(
             $container,
             new FileLocator(__DIR__.'/../../config')
         );
-        $loader->load('services.xml');
+        $loader->load('services.yaml');
     }
 }
