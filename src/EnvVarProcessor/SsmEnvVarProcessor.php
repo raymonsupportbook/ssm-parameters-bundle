698942d898e0a19f97231fe6ef12c7aab29bb5a6
Lucas <lucas.pekel@gmail.com>
2021-04-21 11:56:42 +0200

    Added symfony caching and changed to the correct env syntax

--- src/EnvVarProcessor/SsmEnvVarProcessor.php
+++ src/EnvVarProcessor/SsmEnvVarProcessor.php
@@ -4,25 +4,41 @@ declare(strict_types=1);
 
 namespace Triasto\SsmParametersBundle\EnvVarProcessor;
 
+use AsyncAws\Ssm\Enum\ParameterType;
 use AsyncAws\Ssm\Input\GetParameterRequest;
 use AsyncAws\Ssm\SsmClient;
 use JetBrains\PhpStorm\ArrayShape;
 use Symfony\Component\DependencyInjection\EnvVarProcessorInterface;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
 
 class SsmEnvVarProcessor implements EnvVarProcessorInterface
 {
     public function __construct(
-        private SsmClient $ssmClient
+        private SsmClient $ssmClient,
+        private CacheInterface $cache
     ) {
     }
 
     public function getEnv(string $prefix, string $name, \Closure $getEnv): ?string
-    {
-        return $this->ssmClient->getParameter(
-            [
-                'Name' => $name,
-            ]
-        )->getParameter()?->getValue();
+    { 
+        $name = $getEnv($name);
+
+        return $this->cache->get(
+            sprintf(
+                'triasto-ssm-param-%s',
+                $name
+            ),
+            function (ItemInterface $cacheItem) use ($name) {
+                return $this->ssmClient->getParameter(
+                    [
+                        'Name' => $name,
+                        'WithDecryption' => true,
+                        'Type' => ParameterType::SECURE_STRING,
+                    ]
+                )->getParameter()->getValue();
+            }
+        );
     }
 
     #[ArrayShape(['string' => 'string'])]
